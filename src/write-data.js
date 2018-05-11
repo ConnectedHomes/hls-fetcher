49182db98d6c1943096adc892c4ee3b756b04727
Kamil Badyla <kamilbadyla@gmail.com>
2018-05-11 11:49:42 +0200

    ES6 Auto changes

--- src/write-data.js
+++ src/write-data.js
@@ -1,80 +1,64 @@
-var Promise = require('bluebird');
-var mkdirp = Promise.promisify(require('mkdirp'));
-var request = require('requestretry');
-var fs = Promise.promisifyAll(require('fs'));
-var aesDecrypter = require('aes-decrypter').Decrypter;
-var path = require('path');
+const Promise = require('bluebird');
+const mkdirp = Promise.promisify(require('mkdirp'));
+const request = require('requestretry');
+const fs = Promise.promisifyAll(require('fs'));
+const aesDecrypter = require('aes-decrypter').Decrypter;
+const path = require('path');
 
-var writeFile = function(file, content) {
-  return mkdirp(path.dirname(file)).then(function() {
-    return fs.writeFileAsync(file, content);
-  }).then(function() {
-    console.log('Finished: ' + path.relative('.', file));
+const writeFile = function (file, content) {
+  return mkdirp(path.dirname(file)).then(() => fs.writeFileAsync(file, content)).then(() => {
+    console.log(`Finished: ${path.relative('.', file)}`);
   });
 };
 
-var requestFile = function(uri) {
-  var options = {
-    uri: uri,
+const requestFile = function (uri) {
+  const options = {
+    uri,
     timeout: 60000, // 60 seconds timeout
     encoding: null, // treat all responses as a buffer
-    retryDelay: 1000 // retry 1s after on failure
+    retryDelay: 1000, // retry 1s after on failure
   };
-  return new Promise(function(resolve, reject) {
-    request(options, function(err, response, body) {
+  return new Promise(((resolve, reject) => {
+    request(options, (err, response, body) => {
       if (err) {
         return reject(err);
       }
       return resolve(body);
     });
-  });
+  }));
 };
 
-var toArrayBuffer = function(buffer) {
-    var ab = new ArrayBuffer(buffer.length);
-    var view = new Uint8Array(ab);
-    for (var i = 0; i < buffer.length; ++i) {
-        view[i] = buffer[i];
-    }
-    return ab;
+const toArrayBuffer = function (buffer) {
+  const ab = new ArrayBuffer(buffer.length);
+  const view = new Uint8Array(ab);
+  for (let i = 0; i < buffer.length; ++i) {
+    view[i] = buffer[i];
+  }
+  return ab;
 };
 
-var decryptFile = function(content, encryption) {
-  return new Promise(function(resolve, reject) {
-    var d = new aesDecrypter(toArrayBuffer(content), encryption.bytes, encryption.iv, function(err, bytes) {
-      return resolve(new Buffer(bytes));
-    })
-  });
+const decryptFile = function (content, encryption) {
+  return new Promise(((resolve, reject) => {
+    const d = new aesDecrypter(toArrayBuffer(content), encryption.bytes, encryption.iv, ((err, bytes) => resolve(new Buffer(bytes))));
+  }));
 };
 
-var WriteData = function(decrypt, concurrency, resources) {
-  var inProgress = [];
-  var operations = [];
+const WriteData = function (decrypt, concurrency, resources) {
+  const inProgress = [];
+  const operations = [];
 
-  resources.forEach(function(r) {
+  resources.forEach((r) => {
     if (r.content) {
-      operations.push(function() { return writeFile(r.file, r.content); });
+      operations.push(() => writeFile(r.file, r.content));
     } else if (r.key && decrypt) {
-      operations.push(function() {
-        return requestFile(r.uri).then(function(content) {
-          return decryptFile(content, r.key);
-        }).then(function(content) {
-          return writeFile(r.file, content)
-        });
-      });
+      operations.push(() => requestFile(r.uri).then(content => decryptFile(content, r.key)).then(content => writeFile(r.file, content)));
     } else if (inProgress.indexOf(r.uri) === -1) {
-      operations.push(function() {
-        return requestFile(r.uri).then(function(content) {
-          return writeFile(r.file, content);
-        });
-      });
+      operations.push(() => requestFile(r.uri).then(content => writeFile(r.file, content)));
       inProgress.push(r.uri);
     }
   });
 
-  return Promise.map(operations, function(o) {
-    return Promise.join(o());
-  }, {concurrency: concurrency}).all(function(o) {
+  return Promise.map(operations, o => Promise.join(o()), { concurrency }).all((o) => {
     console.log('DONE!');
     return Promise.resolve();
   });
